4d572fa5930ac226fcccb33b48d7fba1e2090e73
egnarotad <duongdat12092004@gmail.com>
2023-10-16 15:28:39 +0700

    xong ham` Brand

---
+++ Assignment/src/BrandList.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class BrandList extends Brand {
+    
+}
