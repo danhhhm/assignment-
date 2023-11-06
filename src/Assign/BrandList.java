f14f8e0d56e92a084f7f48dd51c0d575f60b8a4e
trandinhtu2004 <138701372+trandinhtu2004@users.noreply.github.com>
2023-11-06 15:30:06 +0700

    dời so sánh bằng hàm lamda sang class, đê tránh gây khó hiểu do chưa được học về lamda

--- src/Assign/BrandList.java
+++ src/Assign/BrandList.java
@@ -205,12 +205,12 @@ public class BrandList extends ArrayList<Brand> {
         System.out.println("Chose(ID/Price/No): ");
         String fieldChoice = sc.next();
         if (fieldChoice.equalsIgnoreCase("ID")) {
-            Collections.sort(this, (b1, b2) -> b1.getBrandID().compareTo(b2.getBrandID()));
+            Collections.sort(this, new BrandIDComparator());
             //b1 b2 là đối tượng được generate từ random brand, b1.getBrandId là lấy id b1 so sánh với id b2
             //Dùng Collections.sort để sắp xếp danh sách trong Java là một cách tiện lợi. Nó  sắp xếp danh sách của bất kỳ đối tượng nào (các đối tượng triển khai giao diện Comparable hoặc cung cấp một Comparator riêng).
             //
         } else if (fieldChoice.equals("price")) {
-            Collections.sort(this, (b1, b2) -> Double.compare(b1.getPrice(), b2.getPrice()));
+            Collections.sort(this, new BrandPriceComparator());
         } else if (fieldChoice.equals("no")) {
         System.out.println("Brands have not been sorted.");
         return;
@@ -223,7 +223,7 @@ public class BrandList extends ArrayList<Brand> {
         System.out.println("Do you want to sort in increasing or decreasing order?");
         System.out.println("Chose(inc/dec): ");
         String orderChoice = sc.next(); // Dòng này xóa bộ đệm (buffer) của Scanner.
-        // "asc" (tăng dần) hoặc "desc" (giảm dần). 
+        // "inc" (tăng dần) hoặc "dec" (giảm dần). 
         //orderChoice = scanner, nó sẽ đợi người dùng nhập một chuỗi từ bàn phím và sau đó trả về chuỗi đó. Chuỗi này sau đó được gán cho biến orderChoice
         if (orderChoice.equals("inc")) {
         System.out.println("Brand has been sorted in increasing order!");
@@ -234,5 +234,20 @@ public class BrandList extends ArrayList<Brand> {
         } else System.out.println("Invalid choice for sorting order.");
     
     }
+    class BrandIDComparator implements Comparator<Brand> {
 
+        @Override
+        public int compare(Brand b1, Brand b2) {
+            return b1.getBrandID().compareTo(b2.getBrandID());
+        }
+    }
+
+    class BrandPriceComparator implements Comparator<Brand> {
+
+        @Override
+        public int compare(Brand b1, Brand b2) {
+            return Double.compare(b1.getPrice(), b2.getPrice());
+        }
+
+    }
 }
